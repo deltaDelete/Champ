f02333fe716861d0cccc9a28c690b648005ff94c
deltaDelete <samansy@yandex.ru>
2024-02-27 13:30:54 +0700

    feat: поддержка .env

--- src/Champ.Front/vite.config.ts
+++ src/Champ.Front/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// @ts-ignore
+export default ({mode}) => {
+    const dotenv = loadEnv(mode, process.cwd());
+    return defineConfig({
+        plugins: [react()],
+        server: {
+            proxy: {
+                "^/api": {
+                    target: dotenv.VITE_BACKEND_ADDRESS
+                }
+            }
+        }
+    })
+
+}
